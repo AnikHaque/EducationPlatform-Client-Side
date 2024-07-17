802f62c9a29fa54ec6e2ffe2abb70ec03003cff7
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-17 20:49:19 +0600

    team section static added

---
+++ src/Componentrs/Teams/Teams.jsx
@@ -0,0 +1,78 @@
+const Teams = () => {
+  return (
+    <div>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
+          <p className="inline-block px-3 py-px mb-4 text-5xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            <span className="text-[#395bdf]">Meet </span>Our Team
+          </p>
+          <p className="text-base text-gray-700 md:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            laboriosam fuga ea molestiae id quam, facilis in inventore labore
+            animi?
+          </p>
+        </div>
+        <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <img
+                className="absolute object-cover w-full h-full rounded-xl"
+                src="https://scontent.fdac24-2.fna.fbcdn.net/v/t39.30808-6/441230021_7619946021389682_159588137589824133_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGrXXlwzb77FDsB7iTIri_GpQejaCh01cGlB6NoKHTVwQvXhMjoS2fwMnUlJDU5OXCAR83RRqaKAtD5a-0er9NF&_nc_ohc=XwP8Xj2oP4sQ7kNvgFTnrU2&_nc_ht=scontent.fdac24-2.fna&oh=00_AYAZ8NzL6plyGwowQ26ydl4naIiYrTvtUE-5SXzuDiZdGw&oe=669DB4F9"
+                alt="Person"
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-2xl font-bold">Kahafil Ora</p>
+              <p className="mb-5 text-md text-gray-800">
+                Managing Director & CEO
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <img
+                className="absolute object-cover w-full h-full rounded-xl"
+                src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/123763045_1118263081921419_2700052177430641771_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeFt28jrpFUQnDkDN_VB3J1twUNNQzp8_eTBQ01DOnz95NoCQ8MZkQqQyRAQZLwhY_wk44DJqhDCl2uIj05ixE8n&_nc_ohc=NH3kc_fP8GgQ7kNvgETRZTV&_nc_ht=scontent.fdac24-1.fna&oh=00_AYA0usJ1maB6MKVCir9okBF8OiwaU_FQX7DFmsCpVrFfIQ&oe=66BF4F24"
+                alt="Person"
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-2xl font-bold">Ekramul Haque Anik</p>
+              <p className="mb-5 text-md text-gray-800">
+                Web & Mobile App Instructor
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <img
+                className="absolute object-cover w-full h-full rounded-xl"
+                src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/95219747_948232512263917_1586787285393735680_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeHFlIfiByiCW0Fcu8uMBV_nOHJUHjYYZWM4clQeNhhlYxU0AWamh2Vtu56ShIyLFKVntZPEUlhm-XgL7JHjNRV9&_nc_ohc=SEyxyOTfEAUQ7kNvgHeviJJ&_nc_ht=scontent.fdac24-1.fna&oh=00_AYCnVj1PMbdBUajjWb6B4HoUnR0cSRd7Z3lZb1T2wiKu5w&oe=66BF43B4"
+                alt="Person"
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-2xl font-bold">Mizanor Rahman</p>
+              <p className="mb-5 text-md text-gray-800">Web Instructor</p>
+            </div>
+          </div>
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <img
+                className="absolute object-cover w-full h-full rounded-xl"
+                src="https://scontent.fdac24-5.fna.fbcdn.net/v/t39.30808-6/439011237_2368348016889647_4510752429131766644_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGN79Vymk0xzP6ZR7HePOeCQj_FGv2o4C9CP8Ua_ajgLyEn8Ybc_96PkpUtrjQh4zTqAFg2wLSWkSkzlxOir5kW&_nc_ohc=yeLjgYcLYxwQ7kNvgEAcRg7&_nc_ht=scontent.fdac24-5.fna&oh=00_AYBNDZ22qcyHuXitEndNvnkA7dUf_VClZ7Mn653aprHWqg&oe=669DB6A9"
+                alt="Person"
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-2xl font-bold">Md. Daud Supto</p>
+              <p className="mb-5 text-md text-gray-800">UI/UX Instructor</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Teams;
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import Awards from "../../Componentrs/Awards/Awards";
 import Career from "../../Componentrs/Career/Career";
 import FeaturedCourses from "../../Componentrs/FeaturedCourses/FeaturedCourses";
 import Hero from "../../Componentrs/Hero/Hero";
+import Teams from "../../Componentrs/Teams/Teams";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const Homepage = () => {
@@ -14,6 +15,7 @@ const Homepage = () => {
       <Awards></Awards>
       <FeaturedCourses></FeaturedCourses>
       <Career></Career>
+      <Teams></Teams>
     </div>
   );
 };
