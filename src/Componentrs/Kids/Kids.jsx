2d8fbc55fa3b7ce05ce7599cdcce865af1e14954
GoinnoviorLimited <info@goinnovior.com>
2024-07-28 15:32:58 +0600

    added

--- src/Componentrs/Kids/Kids.jsx
+++ src/Componentrs/Kids/Kids.jsx
@@ -1,12 +1,16 @@
+import KidsAbout from "./KidsAbout"
 import KidsCard from "./KidsCard"
 import KidsHero from "./KidsHero"
+import KidsTeam from "./KidsTeam"
 
 
 const Kids = () => {
   return (
     <div>
         <KidsHero></KidsHero>
+        <KidsAbout></KidsAbout>
         <KidsCard></KidsCard>
+        <KidsTeam></KidsTeam>
     </div>
   )
 }
