2e76fca36b9b90c55fdc6a28ccf2e375d71d6912
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-29 00:26:40 +0600

    hero section updated

--- src/Componentrs/Hero/Hero.jsx
+++ src/Componentrs/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-[#f4f7ff]">
+    <div className="bg-gradient-to-r from-fuchsia-600 to-blue-600">
       <div className=" py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-32">
         <div className="grid gap-10 lg:grid-cols-2">
           <div
@@ -8,31 +8,29 @@ const Hero = () => {
             className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg"
           >
             <div className="">
-              <h1 className="text-[#f14d5d] font-bold">
-                ALL THE BEST PLATFORM
-              </h1>
+              <h1 className="text-white font-bold">ALL THE BEST PLATFORM</h1>
             </div>
             <div className="max-w-xl mb-6">
               <h2
-                className="max-w-lg text-justify  mb-6 font-sans text-3xl font-bold  text-gray-900 sm:text-4xl sm:leading-none lg:text-6xl 
+                className="max-w-lg text-justify  mb-6 font-sans text-3xl font-bold  text-white sm:text-4xl sm:leading-none lg:text-6xl 
 "
               >
-                Grow <span className="text-[#395bdf]">Your Skills</span>
+                Grow <span className="text-gray-300">Your Skills</span>
                 <br></br>
-                With our <span className="text-[#395bdf]">Training</span>
+                With our <span className="text-gray-300">Training</span>
               </h2>
-              <p className="text-base text-gray-700 md:text-lg">
+              <p className="text-base text-white md:text-lg">
                 Choose from 85,000 online video courses additions published
                 Every month & upgrade your skills. Get started with us.
               </p>
             </div>
             <div>
               <div>
-                <button className="btn btn-active bg-[#395bdf] text-white pl-5">
+                <button className="btn btn-active bg-[#395bdf] text-white font-extrabold pl-5">
                   Explore All Courses{" "}
                   <i className="fa-solid fa-arrow-right-long mt-1 pl-1"></i>
                 </button>
-                <button className="btn btn-outline text-black ml-2">
+                <button className="btn btn-outline text-white font-extrabold ml-2">
                   <i className="fa-solid fa-play text-[#f14d5d]"></i> Watch Our
                   Video
                 </button>
